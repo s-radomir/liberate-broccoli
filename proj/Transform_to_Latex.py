77d228f0e7bd0025c0bae4b79372391caacf6a90
Stepanov Radomir <stepanov-radomir@list.ru>
2022-12-06 00:24:09 +0300

    excel

--- proj/Transform_to_Latex.py
+++ proj/Transform_to_Latex.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import numpy as np
-import csv
 
-def read_csv(file_name):
-    with open(file_name) as file:
-        reader = list(csv.reader(file, delimiter=',',
-                      quotechar=',', quoting=csv.QUOTE_MINIMAL))
-    return reader
+
 
 def make_latex_table_with_borders(data):
     table = []
     table.append("\\begin{table}".replace('//', '\\'))
     table.append("\label{}".replace('/', '\\'))
     table.append('\caption{}'.replace('/', '\\'))
-    leng = len(data[0])
+    leng = len(data.axes[1])
     stroka = 'c'.join(['|' for k in range(leng+1)])
     table.append('\\begin{tabular}{'.replace('//', '\\')+stroka+'}')
     table.append('\hline')
-    for i in range(len(data)):
-        table.append(' & '.join(data[i]) + ' \\\\')
+    for j in range(len(data.axes[0])):
+        for i in range(len(data.axes[1])):
+            table.append(str(data.iat[j, i]))
+            if i != len(data.axes[1])-1:
+                table.append('&')
+        table.append(' \\\\')
         table.append('\hline')
     table.append("\end{tabular}".replace('/', '\\'))
     table.append("\end{table}".replace('/', '\\'))
@@ -29,11 +28,16 @@ def make_latex_table_without_borders(data):
     table.append("\\begin{table}".replace('//', '\\'))
     table.append("\label{}".replace('/', '\\'))
     table.append('\caption{}'.replace('/', '\\'))
-    leng = len(data[0])
+    leng = len(data.axes[1])
     stroka = 'c'.join(['' for k in range(leng+1)])
     table.append('\\begin{tabular}{'.replace('//', '\\')+stroka+'}')
-    for i in range(len(data)):
-        table.append(' & '.join(data[i]) + ' \\\\')
+    for j in range(len(data.axes[0])):
+        for i in range(len(data.axes[1])):
+            table.append(str(data.iat[j, i]))
+            if i != len(data.axes[1])-1:
+                table.append('&')
+        table.append(' \\\\')
+        table.append('\hline')
     table.append("\end{tabular}".replace('/', '\\'))
     table.append("\end{table}".replace('/', '\\'))
     return table
@@ -43,14 +47,15 @@ def make_latex_table_with_separator_between_columns(data):
     table.append("\\begin{table}".replace('//', '\\'))
     table.append("\label{}".replace('/', '\\'))
     table.append('\caption{}'.replace('/', '\\'))
-    leng = len(data[0])
+    leng = len(data.axes[1])
     stroka = 'c'.join(['|' for k in range(leng+1)])
     table.append('\\begin{tabular}{'.replace('//', '\\')+stroka+'}')
     table.append('\hline')
-    for i in range(len(data)):
-        table.append(' & '.join(data[i]) + ' \\\\')
-        if i == 0:
-            table.append('\hline')
+    for j in range(len(data.axes[0])):
+        for i in range(len(data.axes[1])):
+            table.append(str(data.iat[j, i]))
+            if i != len(data.axes[1]) - 1:
+                table.append('&')
     table.append('\hline')
     table.append("\end{tabular}".replace('/', '\\'))
     table.append("\end{table}".replace('/', '\\'))
@@ -66,20 +71,22 @@ def make_latex_table_with_length_columns(data, len_array):
     table.append("\\begin{table}".replace('//', '\\'))
     table.append("\label{}".replace('/', '\\'))
     table.append('\caption{}'.replace('/', '\\'))
-    leng = len(data[0])
+    leng = len(data.axes[1])
     stroka = ''
     for k in len_array:
         stroka += ('m{' + str(k) + 'cm}|' )
     table.append('\\begin{tabular}{'.replace('//', '\\')+'|' + stroka+'}')
     table.append('\hline')
-    for i in range(len(data)):
-        table.append(' & '.join(data[i]) + ' \\\\')
-        table.append('\hline')
+    for j in range(len(data.axes[0])):
+        for i in range(len(data.axes[1])):
+            table.append(str(data.iat[j, i]))
+            if i != len(data.axes[1]) - 1:
+                table.append('&')
     table.append("\end{tabular}".replace('/', '\\'))
     table.append("\end{table}".replace('/', '\\'))
     return table
 
 len_array = [2, 3, 4]
-A = read_csv(r"C:\Users\user\Downloads\23.csv")
-for i in make_latex_table_with_length_columns(A, len_array):
+Q = pd.read_excel(r'C:\Users\user\liberate-broccoli\proj\l.xlsx')
+for i in make_latex_table_with_borders(Q):
     print(i)
